b62c18f3ba2f133034285dfab40b9781b820379b
montalvas <lucas_montalvani@hotmail.com>
2022-05-19 16:22:10 -0300

    recipe-list e recipe-home com conteudo dinamico

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'recipes'
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/<int:pk>', views.recipe, name='recipe')
+    path('recipes/<int:pk>/', views.recipe, name='recipe')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .utils.recipes.factory import make_recipe
+
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    context = {
+        'recipes': [make_recipe() for _ in range(10)]
+    }
+    
+    return render(request, 'recipes/home.html', context)
 
 def recipe(request, pk):
-    return render(request, 'recipes/recipe.html')
+    context = {
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    }
+    
+    return render(request, 'recipes/recipe.html', context)
